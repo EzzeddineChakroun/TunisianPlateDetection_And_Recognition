b1ce50f2a26b1e2f414a7b5575fe07b33d5c27ed
EzzeddineChakroun <97363850+EzzeddineChakroun@users.noreply.github.com>
2022-12-14 10:02:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from detect_plate import detect_plate
-from ocr_plate import plate_ocr
+import easyocr
 import os
 
+from detect_plate import detect_plate
+from ocr_plate import plate_ocr
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("testing_images", help="Path to the folder containing the images to test", default="images")
+args = parser.parse_args()
+
 #Testing Folder 
-testing_images = "images" 
-for image_name in os.listdir(testing_images):
-    image_path = os.path.join(testing_images,image_name)
+for image_name in os.listdir(args.testing_images):
+    image_path = os.path.join(args.testing_images,image_name)
     detected_plate = detect_plate(image_path)
     if (detected_plate is None):
         print("image_name: ",image_name,". No plate detected ")
